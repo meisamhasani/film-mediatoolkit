03a8c77d411a4c68fea7abae6a891f03e662f264
Hassan Hashemi <HassanHashemi@yahoo.com>
2018-10-13 14:42:09 +0330

    Initial commit

---
+++ Basic/Program.cs
@@ -0,0 +1,21 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = new MediaFile { Filename = @"e:\1.mp4" };
+
+            using (var engine = new Engine(@"E:\cmd\ffmpeg.exe"))
+            {
+                engine.GetMetadata(inputFile);
+            }
+
+            Console.WriteLine(inputFile.Metadata.Duration);
+        }
+    }
+}
