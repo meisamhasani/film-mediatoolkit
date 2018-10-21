6f23a9e019980d16122317a13d3cc39e0d078b32
Hassan Hashemi <HassanHashemi@yahoo.com>
2018-10-21 13:21:29 +0330

    Completing serializer

---
+++ test/Tests/CommandBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using MediaToolkit;
+using MediaToolkit.HLSOptions;
+using MediaToolkit.Model;
+using Xunit;
+
+namespace Tests
+{
+    public class CommandBuilderTests
+    {
+        [Fact]
+        public void Should_Create_Default()
+        {
+            var parameters = new EngineParameters()
+            {
+                HLSOptions = new HLSGeneratingOptions(@"D:\test\", "360p_%03d.ts", "360p.m3u8"),
+                InputFile = new MediaFile(@"E:\1.mp4"),
+                Task = FFmpegTask.GenerateHLS
+            };
+
+            var result = CommandBuilder.GetHLS(parameters);
+        }
+    }
+}
