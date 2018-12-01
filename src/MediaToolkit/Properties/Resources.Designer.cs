ae0d61ea0a9f1d639860cc2f44b22341adb4924a
Hassan Hashemi <HassanHashemi@yahoo.com>
2018-12-01 11:44:38 +0330

    update version

--- src/MediaToolkit/Properties/Resources.Designer.cs
+++ src/MediaToolkit/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MediaToolkit.Properties
-{
-
-
+namespace MediaToolkit.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,7 +22,7 @@ namespace MediaToolkit.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace MediaToolkit.Properties
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MediaToolkit.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace MediaToolkit.Properties
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace MediaToolkit.Properties
         /// <summary>
         ///   Looks up a localized string similar to Input file not found.
         /// </summary>
-        internal static string Exception_Media_Input_File_Not_Found {
+        public static string Exception_Media_Input_File_Not_Found {
             get {
                 return ResourceManager.GetString("Exception_Media_Input_File_Not_Found", resourceCulture);
             }
@@ -72,7 +72,7 @@ namespace MediaToolkit.Properties
         /// <summary>
         ///   Looks up a localized string similar to FFmpeg process is not running..
         /// </summary>
-        internal static string Exceptions_FFmpeg_Process_Not_Running {
+        public static string Exceptions_FFmpeg_Process_Not_Running {
             get {
                 return ResourceManager.GetString("Exceptions_FFmpeg_Process_Not_Running", resourceCulture);
             }
@@ -81,7 +81,7 @@ namespace MediaToolkit.Properties
         /// <summary>
         ///   Looks up a localized string similar to FFMpeg GZip stream is null.
         /// </summary>
-        internal static string Exceptions_Null_FFmpeg_Gzip_Stream {
+        public static string Exceptions_Null_FFmpeg_Gzip_Stream {
             get {
                 return ResourceManager.GetString("Exceptions_Null_FFmpeg_Gzip_Stream", resourceCulture);
             }
@@ -90,7 +90,7 @@ namespace MediaToolkit.Properties
         /// <summary>
         ///   Looks up a localized string similar to MediaToolkit.Resources.FFmpeg.exe.gz.
         /// </summary>
-        internal static string FFmpegManifestResourceName {
+        public static string FFmpegManifestResourceName {
             get {
                 return ResourceManager.GetString("FFmpegManifestResourceName", resourceCulture);
             }
@@ -99,7 +99,7 @@ namespace MediaToolkit.Properties
         /// <summary>
         ///   Looks up a localized string similar to ffmpeg.
         /// </summary>
-        internal static string FFmpegProcessName {
+        public static string FFmpegProcessName {
             get {
                 return ResourceManager.GetString("FFmpegProcessName", resourceCulture);
             }
