6e3316f5ec2b19c5f3e32a1eed87bcc04fc1fc0d
Hassan Hashemi <HassanHashemi@yahoo.com>
2019-02-24 13:26:54 +0330

    update versions

--- src/MediaToolkit.Web/IMediaEngine.cs
+++ src/MediaToolkit.Web/IMediaEngine.cs
@@ -1,12 +1,14 @@
 ﻿using MediaToolkit.HLSOptions;
 using MediaToolkit.Model;
 using MediaToolkit.Options.GIF;
+using MediaToolkit.Options.Storyboard;
 using System.Threading.Tasks;
 
 namespace MediaToolkit.Web
 {
     public interface IMediaEngine
     {
+        Task Storyboard(string inputFile, StoryBoardOptions options);
         Task<Metadata> Metadata(string path);
         Task<bool> IsValid(string physicalPath);
         Task GenerateHLS(EngineParameters parameters);
